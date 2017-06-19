cdc224877239980e429d8b7d773e42feb7b381d7
Bryan Jackson <bjack13@gmail.com>
2017-06-19 10:00:16 -0600

    set up initial props test on card Component

--- src/Card.js
+++ src/Card.js
@@ -3,19 +3,24 @@ import './Card.css';
 
 class Card extends Component {
   render() {
+    let name = this.props.name || "test";
+    let imageURL = this.props.imageURL || "http://localhost:3008/darth_vader.jpg";
+    let birthday = this.props.birthday || "test";
+    let homeWorld = this.props.homeWorld || "test";
+
     return (
       <div className='card'>
         <div className='card-content'>
-          	<div className='card-name'>Darth Vader</div>
-          	<img src='http://localhost:3008/darth_vader.jpg' alt='profile'/>
+          	<div className='card-name'>{name}</div>
+          	<img src={imageURL} alt='profile'/>
             <p>
                 <span>Birthday:</span>
-                <span>41.9BBY</span>
+                <span>{birthday}</span>
             </p>
             <p>
                 {/* Note that in order to get the homeworld's name, you have to get the planet name from a different endpoint than the people */}
                 <span>Homeworld:</span>
-                <span>Tatooine</span>
+                <span>{homeWorld}</span>
             </p>
         </div>
     </div>
